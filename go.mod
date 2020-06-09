203a414d6df6971fc351c0f4e9a14d330ef34c82
ivangonzalezacuna <ivangonzalezacuna@gmail.com>
2020-06-09 16:55:26 +0200

    WIP: Solving issue with txFlag

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/cdipaolo/goml v0.0.0-20190412180403-e1f51f713598
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/sirupsen/logrus v1.6.0
+	github.com/spf13/viper v1.7.0
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 )
--- main.go
+++ main.go
@@ -14,11 +14,14 @@ import (
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 var (
 	// Initial array of data received from each sensor
 	receivedData datafusion.CollectData
+	// Auxiliar array of data received from each sensor (to avoid re-write)
+	receivedDataFinal datafusion.CollectData
 	// Struct generated from the received data of each sensor
 	generatedData datafusion.JoinedData
 	// Array of data for each detected user by the camera and/or rfid reader
@@ -30,30 +33,49 @@ var (
 )
 
 var c mqtt.Client
-var txFlag bool = false
+var txFlag bool
+var count int
 
 var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	if !txFlag {
-		txFlag = true
-		byteData, err := json.Marshal(txFlag)
+	if !txFlag && count == 0 {
+		count++
+		value := true
+		byteData, err := json.Marshal(value)
 		if err != nil {
 			log.Infof(err.Error())
 			return
 		}
+		log.Infof("Activating flag! Because of: %v", string(msg.Payload()))
 		token := c.Publish("Node/Flag", 0, false, byteData)
 		if token.Wait() && token.Error() != nil {
 			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
 		}
+
 		go func() {
 			time.Sleep(200 * time.Millisecond)
-			txFlag = false
+			value := false
+			byteData, err := json.Marshal(value)
+			if err != nil {
+				log.Infof(err.Error())
+				return
+			}
+			log.Infof("Deactivating flag after 200ms! Because of: %v", string(msg.Payload()))
+			token := c.Publish("Node/Flag", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+			receivedDataFinal = receivedData
+			log.Infof("Camera size: %v\nPresence size: %v\nRfid size: %v\nWifi size: %v\n",
+				len(receivedDataFinal.Camera), len(receivedDataFinal.Presence), len(receivedDataFinal.Rfid), len(receivedDataFinal.Wifi))
+			makePredictions()
+			count--
 		}()
-		log.Infof("We need to activate the flag!")
+	} else if txFlag {
+		log.Tracef("Received: %v", string(msg.Payload()))
+		split := strings.Split(msg.Topic(), "/")
+		sensor := split[len(split)-1]
+		receivedData.AddNewValue(msg.Payload(), strings.ToLower(sensor))
 	}
-	log.Debugf("Received: %v", string(msg.Payload()))
-	split := strings.Split(msg.Topic(), "/")
-	sensor := split[len(split)-1]
-	receivedData.AddNewValue(msg.Payload(), sensor)
 }
 
 var ff mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
@@ -62,40 +84,72 @@ var ff mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 		log.Errorf(err.Error())
 		return
 	}
-	log.Infof("MSG: %v, TOPIC: %v", txFlag, msg.Topic())
+	log.Infof("MSG: %v, TYPE: %T, TOPIC: %v", txFlag, txFlag, msg.Topic())
 }
 
 func init() {
 	log.SetLevel(log.DebugLevel)
+	txFlag = false
+	count = 0
 
-	// receivedData = datafusion.CollectData{}
+	readConfig()
+	viper.SetDefault("mqtt.server", "tcp://127.0.0.1:1883")
+	server := viper.GetString("mqtt.server")
+	viper.SetDefault("mqtt.clientId", "test-client")
+	clientID := viper.GetString("mqtt.clientId")
+	viper.SetDefault("mqtt.keepAlive", 2)
+	keepAlive := viper.GetInt("mqtt.keepAlive")
+	viper.SetDefault("mqtt.pingTimeout", 1)
+	pingTimeout := viper.GetInt("mqtt.pingTimeout")
 
-	// trainData, err := ml.LoadTrainDataFromCSV("./data/trackDataTrain.csv", "./data/trackDataTrain.csv")
-	// if err != nil {
-	// 	log.Errorf(err.Error())
-	// 	os.Exit(400)
-	// }
-
-	// trainModel, err = trainData.CreateBestModel()
-	// if err != nil {
-	// 	log.Errorf(err.Error())
-	// 	os.Exit(400)
-	// }
-
-	// log.Debugf("ModelData: %#v", trainModel)
-}
-
-func main() {
-	opts := mqtt.NewClientOptions().AddBroker("tcp://127.0.0.1:1883").SetClientID("test-client")
-	opts.SetKeepAlive(2 * time.Second)
-	opts.SetPingTimeout(1 * time.Second)
+	opts := mqtt.NewClientOptions().AddBroker(server).SetClientID(clientID)
+	opts.SetKeepAlive(time.Duration(keepAlive) * time.Second)
+	opts.SetPingTimeout(time.Duration(pingTimeout) * time.Second)
 
 	log.Infof("Connecting to MQTT broker...")
 	c = mqtt.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		log.Errorf(token.Error().Error())
+		os.Exit(400)
+	}
+	err := generateTrainData()
+	if err != nil {
+		log.Errorf(err.Error())
+		os.Exit(400)
+	}
+	receivedData = datafusion.CollectData{}
+}
+
+func generateTrainData() error {
+	viper.SetDefault("ml.trainFile", "")
+	trainFile := viper.GetString("ml.trainFile")
+	viper.SetDefault("ml.testFile", "")
+	testFile := viper.GetString("ml.testFile")
+	trainData, err := ml.LoadTrainDataFromCSV(trainFile, testFile)
+	if err != nil {
+		return err
 	}
 
+	trainModel, err = trainData.CreateBestModel()
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("ModelData: %#v", trainModel)
+	return nil
+}
+
+func readConfig() {
+	cfgFile := "config.toml"
+	viper.SetConfigFile(cfgFile)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Errorf("[Init] Unable to read config from file %s: %s", cfgFile, err.Error())
+	} else {
+		log.Infof("[Init] Read configuration from file %s", cfgFile)
+	}
+}
+
+func main() {
 	log.Infof("Subscribing to MQTT Topic...")
 	if token := c.Subscribe("Node/Sensor/+", 0, f); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
@@ -107,62 +161,66 @@ func main() {
 		os.Exit(1)
 	}
 
-	// // Send some data for each sensor (random data)
-	go auxSendCamera()
-	go auxSendPresence()
-	go auxSendRfid()
-	go auxSendWifi()
+	k := 0
+	for k < 3 {
+		k++
+		// Send data for each sensor (random data)
+		go auxSendCamera()
+		go auxSendPresence()
+		go auxSendRfid()
+		go auxSendWifi()
+		time.Sleep(5 * time.Second)
+	}
 
-	// time.Sleep(4 * time.Second)
-	log.Infof("FINISHED DATA TRANSMISSION")
+	// In order to keep the code running. Provisional
+	fmt.Scanln()
+}
 
-	// t1 := time.Now()
+func makePredictions() {
+	t1 := time.Now()
 
-	// // Calculate the AVG result / list of results from the whole data received from each sensor
-	// generatedData = datafusion.JoinedData{}
-	// generatedData.Camera.Sensor = "camera"
-	// generatedData.GetFinalCameraValues(receivedData)
-	// generatedData.Presence.Sensor = "presence"
-	// generatedData.GetFinalPresenceValues(receivedData)
-	// generatedData.Rfid.Sensor = "rfid"
-	// generatedData.GetFinalRfidValues(receivedData)
-	// generatedData.Wifi.Sensor = "camera"
-	// generatedData.GetFinalWifiValues(receivedData)
+	// Calculate the AVG result / list of results from the whole data received from each sensor
+	generatedData = datafusion.JoinedData{}
+	generatedData.Camera.Sensor = "camera"
+	generatedData.GetFinalCameraValues(receivedDataFinal)
+	generatedData.Presence.Sensor = "presence"
+	generatedData.GetFinalPresenceValues(receivedDataFinal)
+	generatedData.Rfid.Sensor = "rfid"
+	generatedData.GetFinalRfidValues(receivedDataFinal)
+	generatedData.Wifi.Sensor = "camera"
+	generatedData.GetFinalWifiValues(receivedDataFinal)
 
-	// log.Debugf("CAMERA -> %#v", generatedData.Camera)
-	// log.Debugf("PRESENCE -> %#v", generatedData.Presence)
-	// log.Debugf("RFID -> %#v", generatedData.Rfid)
-	// log.Debugf("WiFi -> %#v", generatedData.Wifi)
+	log.Debugf("CAMERA -> %#v", generatedData.Camera)
+	log.Debugf("PRESENCE -> %#v", generatedData.Presence)
+	log.Debugf("RFID -> %#v", generatedData.Rfid)
+	log.Debugf("WiFi -> %#v", generatedData.Wifi)
 
-	// // Obtain a final list with the data to send to the ML algorithm
-	// predictionDataStruct = datafusion.FinalData{}
-	// predictionDataStruct.ObtainFinalData(generatedData)
+	// Obtain a final list with the data to send to the ML algorithm
+	predictionDataStruct = datafusion.FinalData{}
+	predictionDataStruct.ObtainFinalData(generatedData)
 
-	// t2 := time.Now()
+	t2 := time.Now()
 
-	// result, _ := json.MarshalIndent(predictionDataStruct, "", "  ")
-	// log.Infof(string(result))
+	result, _ := json.MarshalIndent(predictionDataStruct, "", "  ")
+	log.Infof(string(result))
 
-	// log.Infof("Time doing join and calculating final data array: %v", t2.Sub(t1))
+	log.Infof("Time doing join and calculating final data array: %v", t2.Sub(t1))
 
-	// // predictionData := predictionDataStruct.To2DFloatArray()
+	predictionData := predictionDataStruct.To2DFloatArray()
 	// var predictionData [][]float64
 	// data1 := []float64{76.32, 1.43, 1.43, 21.65, 12.98}
 	// data2 := []float64{76.32, 1.43, 71.43, 75.65, 12.98}
 	// data3 := []float64{25.32, 0.43, 1.43, 21.65, 35.98}
 	// data4 := []float64{28.32, 1.43, 1.43, 21.65, 89.98}
 	// predictionData = append(predictionData, data1, data2, data3, data4)
-	// log.Infof("Obtained 2D Array to predict: %v", predictionData)
+	log.Infof("Obtained 2D Array to predict: %v", predictionData)
 
-	// prediction, err := trainModel.MakePrediction(predictionData)
-	// if err != nil {
-	// 	log.Errorf(err.Error())
-	// }
+	prediction, err := trainModel.MakePrediction(predictionData)
+	if err != nil {
+		log.Errorf(err.Error())
+	}
 
-	// log.Infof("Result of prediction: %v", prediction)
-
-	// In order to keep the code running. Provisional
-	fmt.Scanln()
+	log.Infof("Result of prediction: %v", prediction)
 }
 
 func auxSendCamera() {
@@ -180,11 +238,16 @@ func auxSendCamera() {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		log.Infof("Flag value %v", txFlag)
+		if txFlag {
+			token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Camera data")
 		}
-		// dataReceiver(data, data["sensor"].(string))
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
 	for i < 40 {
@@ -199,10 +262,15 @@ func auxSendCamera() {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		if txFlag {
+			token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Camera data")
 		}
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
 	for i < 100 {
@@ -217,17 +285,22 @@ func auxSendCamera() {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		if txFlag {
+			token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Camera data")
 		}
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
 }
 
 func auxSendPresence() {
 	i := 0
-	for i < 10 {
+	for i < 49 {
 		timestamp := 123456789 + i
 		detection := (i%7 == 0) // || (i%3 == 0)
 		data := map[string]interface{}{
@@ -240,10 +313,15 @@ func auxSendPresence() {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Presence", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		if txFlag || (!txFlag && detection) {
+			token := c.Publish("Node/Sensor/Presence", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Presence data")
 		}
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
 }
@@ -254,10 +332,11 @@ func auxSendRfid() {
 	max := 100
 	for i < 2 {
 		timestamp := 123456789 + i
+		power := float64(rand.Intn(max-min) + min)
 		data := map[string]interface{}{
 			"sensor":    "rfid",
 			"timestamp": strconv.Itoa(timestamp),
-			"power":     float64(rand.Intn(max-min) + min),
+			"power":     power,
 			"person":    7,
 		}
 		byteData, err := json.Marshal(data)
@@ -265,18 +344,24 @@ func auxSendRfid() {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		if txFlag || (!txFlag && (power >= 60)) {
+			token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Rfid data")
 		}
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
 	for i < 3 {
 		timestamp := 123456789 + i
+		power := float64(rand.Intn(max-min) + min)
 		data := map[string]interface{}{
 			"sensor":    "rfid",
 			"timestamp": strconv.Itoa(timestamp),
-			"power":     float64(rand.Intn(max-min) + min),
+			"power":     power,
 			"person":    5,
 		}
 		byteData, err := json.Marshal(data)
@@ -284,18 +369,24 @@ func auxSendRfid() {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		if txFlag || (!txFlag && (power >= 60)) {
+			token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Rfid data")
 		}
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
 	for i < 70 {
 		timestamp := 123456789 + i
+		power := float64(rand.Intn(max-min) + min)
 		data := map[string]interface{}{
 			"sensor":    "rfid",
 			"timestamp": strconv.Itoa(timestamp),
-			"power":     float64(rand.Intn(max-min) + min),
+			"power":     power,
 			"person":    6,
 		}
 		byteData, err := json.Marshal(data)
@@ -303,10 +394,15 @@ func auxSendRfid() {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		if txFlag || (!txFlag && (power >= 60)) {
+			token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Rfid data")
 		}
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
 }
@@ -317,30 +413,26 @@ func auxSendWifi() {
 		timestamp := 123456789 + i
 		min := 0
 		max := 3
+		devices := rand.Intn(max-min) + min
 		data := map[string]interface{}{
 			"sensor":           "wifi",
 			"timestamp":        strconv.Itoa(timestamp),
-			"connecteddevices": rand.Intn(max-min) + min,
+			"connecteddevices": devices,
 		}
 		byteData, err := json.Marshal(data)
 		if err != nil {
 			log.Errorf(err.Error())
 			return
 		}
-		token := c.Publish("Node/Sensor/Wifi", 0, false, byteData)
-		if token.Wait() && token.Error() != nil {
-			panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+		if txFlag || (!txFlag && (devices >= 2)) {
+			token := c.Publish("Node/Sensor/Wifi", 0, false, byteData)
+			if token.Wait() && token.Error() != nil {
+				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
+			}
+		} else {
+			log.Warnf("Unable to send Wifi data")
 		}
+		time.Sleep(1 * time.Millisecond)
 		i++
 	}
-}
-
-func dataReceiver(u interface{}, topic string) {
-	byteData, err := json.Marshal(u)
-	if err != nil {
-		log.Errorf(err.Error())
-		return
-	}
-	log.Trace("Received: %v", string(byteData))
-	receivedData.AddNewValue(byteData, topic)
 }
