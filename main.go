64cdfd00a34574781c17dc718183ff905abaf17d
ivangonzalezacuna <ivangonzalezacuna@gmail.com>
2020-06-11 17:08:39 +0200

    Send data from external code

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -37,7 +35,7 @@ var txFlag bool
 var count int
 
 var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	if !txFlag && count == 0 {
+	if !txFlag && count == 0 && trainModel.Model != nil {
 		count++
 		value := true
 		byteData, err := json.Marshal(value)
@@ -170,17 +168,6 @@ func subscribeToTopics() error {
 }
 
 func main() {
-	k := 0
-	for k < 30 {
-		k++
-		// Send data for each sensor (random data)
-		go auxSendCamera()
-		go auxSendPresence()
-		go auxSendRfid()
-		go auxSendWifi()
-		time.Sleep(5 * time.Second)
-	}
-
 	// In order to keep the code running. Provisional
 	fmt.Scanln()
 }
@@ -230,217 +217,4 @@ func makePredictions() {
 	}
 
 	log.Infof("[Prediction] Result of prediction: %v", prediction)
-}
-
-func auxSendCamera() {
-	i := 0
-	for i < 20 {
-		timestamp := 123456789 + i
-
-		data := map[string]interface{}{
-			"sensor":    "camera",
-			"timestamp": strconv.Itoa(timestamp),
-			"person":    5,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag {
-			token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Camera data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
-	for i < 40 {
-		timestamp := 123456789 + i
-		data := map[string]interface{}{
-			"sensor":    "camera",
-			"timestamp": strconv.Itoa(timestamp),
-			"person":    7,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag {
-			token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Camera data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
-	for i < 60 {
-		timestamp := 123456789 + i
-		data := map[string]interface{}{
-			"sensor":    "camera",
-			"timestamp": strconv.Itoa(timestamp),
-			"person":    9,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag {
-			token := c.Publish("Node/Sensor/Camera", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Camera data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
-}
-
-func auxSendPresence() {
-	i := 0
-	for i < 60 {
-		timestamp := 123456789 + i
-		detection := (i%7 == 0) // || (i%3 == 0)
-		data := map[string]interface{}{
-			"sensor":    "presence",
-			"timestamp": strconv.Itoa(timestamp),
-			"detection": detection,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag || (!txFlag && detection) {
-			token := c.Publish("Node/Sensor/Presence", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Presence data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
-}
-
-func auxSendRfid() {
-	i := 0
-	min := 0
-	max := 100
-	for i < 8 {
-		timestamp := 123456789 + i
-		power := float64(rand.Intn(max-min) + min)
-		data := map[string]interface{}{
-			"sensor":    "rfid",
-			"timestamp": strconv.Itoa(timestamp),
-			"power":     power,
-			"person":    7,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag || (!txFlag && (power >= 60)) {
-			token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Rfid data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
-	for i < 20 {
-		timestamp := 123456789 + i
-		power := float64(rand.Intn(max-min) + min)
-		data := map[string]interface{}{
-			"sensor":    "rfid",
-			"timestamp": strconv.Itoa(timestamp),
-			"power":     power,
-			"person":    5,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag || (!txFlag && (power >= 60)) {
-			token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Rfid data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
-	for i < 60 {
-		timestamp := 123456789 + i
-		power := float64(rand.Intn(max-min) + min)
-		data := map[string]interface{}{
-			"sensor":    "rfid",
-			"timestamp": strconv.Itoa(timestamp),
-			"power":     power,
-			"person":    6,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag || (!txFlag && (power >= 60)) {
-			token := c.Publish("Node/Sensor/Rfid", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Rfid data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
-}
-
-func auxSendWifi() {
-	i := 0
-	for i < 20 {
-		timestamp := 123456789 + i
-		min := 0
-		max := 3
-		devices := rand.Intn(max-min) + min
-		data := map[string]interface{}{
-			"sensor":           "wifi",
-			"timestamp":        strconv.Itoa(timestamp),
-			"connecteddevices": devices,
-		}
-		byteData, err := json.Marshal(data)
-		if err != nil {
-			log.Errorf(err.Error())
-			return
-		}
-		if txFlag || (!txFlag && (devices >= 2)) {
-			token := c.Publish("Node/Sensor/Wifi", 0, false, byteData)
-			if token.Wait() && token.Error() != nil {
-				panic(fmt.Sprintf("Error publishing: %v", token.Error()))
-			}
-		} else {
-			log.Warnf("Unable to send Wifi data")
-		}
-		time.Sleep(1 * time.Millisecond)
-		i++
-	}
 }
